3369e3cbf0c43e28324e6ebb102a17436f4bf094
AvRusem <avzalor@gmail.com>
2025-01-08 19:40:25 +0300

    Add CourierTypes and Parse/Serialize/ToPg

---
+++ src/couriers/courier_types.hpp
@@ -0,0 +1,48 @@
+#ifndef LAVKA_COURIERS_COURIER_TYPES_HPP
+#define LAVKA_COURIERS_COURIER_TYPES_HPP
+
+#include <string_view>
+
+#include <userver/formats/parse/to.hpp>
+#include <userver/utils/trivial_map.hpp>
+#include <userver/formats/json/value.hpp>
+#include <userver/formats/serialize/to.hpp>
+#include <userver/formats/serialize/common_containers.hpp>
+#include <userver/storages/postgres/io/io_fwd.hpp>
+#include <userver/storages/postgres/io/enum_types.hpp>
+
+namespace lavka {
+
+enum class CourierType {
+  kFoot,
+  kBike,
+  kAuto
+};
+
+static constexpr userver::utils::TrivialBiMap kCourierTypesMap([](auto selector) {
+    return selector().Case(CourierType::kFoot, "FOOT").Case(CourierType::kBike, "BIKE").Case(CourierType::kAuto, "AUTO");
+  });
+
+template <class Value>
+CourierType Parse(const Value& data, userver::formats::parse::To<CourierType>) {
+  return userver::utils::ParseFromValueString(data, kCourierTypesMap);
+}
+
+template <class Value>
+Value Serialize(const CourierType& data, userver::formats::serialize::To<Value>) {
+  return typename Value::Builder(kCourierTypesMap.TryFind(data)).ExtractValue();
+}
+  
+} // namespace lavka
+
+namespace userver::storages::postgres::io {
+
+template <>
+struct CppToUserPg<lavka::CourierType> {
+  static constexpr DBTypeName postgres_name = "couriers.delivery";
+  static constexpr userver::utils::TrivialBiMap enumerators = lavka::kCourierTypesMap;
+};
+  
+} // namespace userver::storages::postgres::io
+
+#endif  // LAVKA_COURIERS_COURIER_TYPES_HPP
---
+++ src/couriers/tests/courier_types_test.cpp
@@ -0,0 +1,49 @@
+#include "../courier_types.hpp"
+
+#include <userver/utest/utest.hpp>
+#include <userver/formats/json/value.hpp>
+#include <userver/formats/json/value_builder.hpp>
+
+using namespace userver;
+
+UTEST(CourierTypes, SuccessJsonParse) {
+  using lavka::CourierType;
+
+  formats::json::Value json = formats::json::FromString(R"({
+  "foot": "FOOT",
+  "bike": "BIKE",
+  "auto": "AUTO"
+  })");
+
+  EXPECT_EQ(json["foot"].As<CourierType>(), CourierType::kFoot);
+  EXPECT_EQ(json["bike"].As<CourierType>(), CourierType::kBike);
+  EXPECT_EQ(json["auto"].As<CourierType>(), CourierType::kAuto);
+}
+
+UTEST(CourierTypes, ExceptionJsonParse) {
+  using lavka::CourierType;
+
+  formats::json::Value json = formats::json::FromString(R"({
+  "invalid": "INVALID"
+  })");
+
+  EXPECT_THROW(json["invalid"].As<CourierType>(), formats::json::Exception);
+}
+
+UTEST(CourierTypes, SuccessSerialize) {
+  using lavka::CourierType;
+
+  CourierType foot = CourierType::kFoot;
+  CourierType bike = CourierType::kBike;
+  CourierType auto_v = CourierType::kAuto;
+
+  formats::json::ValueBuilder builderJson;
+
+  builderJson["foot"] = foot;
+  builderJson["bike"] = bike;
+  builderJson["auto"] = auto_v;
+  auto json = builderJson.ExtractValue();
+  EXPECT_EQ(json["foot"].As<std::string>(), "FOOT");
+  EXPECT_EQ(json["bike"].As<std::string>(), "BIKE");
+  EXPECT_EQ(json["auto"].As<std::string>(), "AUTO");
+}
